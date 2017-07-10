b64bab95b38a8aa651bbd611cb120eaf33fa537d
Joyce <1843019320@qq.com>
2017-07-10 17:08:11 +0800

    Initial Commit

---
+++ coreBluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreBluetooth
+//
+//  Created by Joyce on 17/7/10.
+//  Copyright © 2017年 Joyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreBluetooth
+//
+//  Created by Joyce on 17/7/10.
+//  Copyright © 2017年 Joyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
